f9a8b37e1d029e2decf04e2aa5671483b9c924b9
DylanG-Dev <gametdylan@gmail.com>
2025-05-10 08:19:59 +0200

    Update log.sql
    maj du script de la création de la bdd

--- Bdd/log.sql
+++ Bdd/log.sql
@@ -1,8 +1,18 @@
-ALTER TABLE `loueur` ADD `motdepasse` VARCHAR(255);
-INSERT INTO `loueur`(`id`, `nom`, `appelsKO`, `timeouts`, `motdepasse`) 
-VALUES ('2000','administrateur','0','0','administrateur');
+CREATE DATABASE analyselog;
+USE analyselog;
 
-ALTER TABLE `loueur` ADD `pays` VARCHAR(255);
-ALTER TABLE `loueur` ADD `email` VARCHAR(255);
-ALTER TABLE `loueur` ADD `numTel` VARCHAR(255);
-ALTER TABLE `loueur` ADD `date` DATETIME DEFAULT CURRENT_TIMESTAMP;
+DROP TABLE IF EXISTS `loueur`;
+CREATE TABLE IF NOT EXISTS `loueur` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `appelsKO` int NOT NULL,
+  `timeouts` int NOT NULL,
+  `motdepasse` varchar(255) DEFAULT NULL,
+  `pays` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `numTel` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `loueur`(`id`, `nom`, `appelsKO`, `timeouts`, `motdepasse`)
+VALUES ('2000','administrateur','0','0','administrateur');
